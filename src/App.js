2fe6014120e2ad1a2e128001cdc1dfd45f57f1b8
Roman <r.levyts@gmail.com>
2018-04-22 20:21:29 -0400

    fix overlapping statusbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { StatusBar, View, Platform, StyleSheet, Text } from 'react-native';
 import AppWithNavigationState from './config/routes';
 import store from './config/store';
+import { Colors } from './constants/styles';
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+const MyStatusBar = ({backgroundColor, ...props}) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 export default App = () => (
   <Provider store={store}>
-    <AppWithNavigationState/>
+    <View style={styles.container}>
+      <MyStatusBar backgroundColor={Colors.appGrey} barStyle="light-content" />
+      <AppWithNavigationState/>
+    </View>
   </Provider>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.appGrey,
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  }
+});
