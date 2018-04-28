e768e04571cad6251baebc5ba8677ab5e3990af8
Roman <r.levyts@gmail.com>
2018-04-28 03:32:37 -0400

    work on styling for settings and create portfolio screens

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -11,7 +11,7 @@ import {connect, dispatch} from 'react-redux'
 import { logout } from '../actions/auth'
 
 import CustomButton from '../components/CustomButton'
-
+import { Colors } from '../constants/styles'
 type Props = {};
 
 class Settings extends Component<Props> {
@@ -22,26 +22,28 @@ class Settings extends Component<Props> {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 20, marginBottom: 30}}>Settings</Text>
-        <View style={styles.settingOptionWrapper}>
-          <Text style={styles.settingOption}>Banking</Text><Text style={styles.settingArrow}>></Text>
-        </View>
-        <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>
-        <View style={styles.settingOptionWrapper}>
-          <Text style={styles.settingOption}>History</Text><Text style={styles.settingArrow}>></Text>
-        </View>
-        <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>        
-        <View style={styles.settingOptionWrapper}>
-          <Text style={styles.settingOption}>General</Text><Text style={styles.settingArrow}>></Text>
-        </View>
-        <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>        
-        <View style={styles.settingOptionWrapper}>
-          <Text style={styles.settingOption}>Help</Text><Text style={styles.settingArrow}>></Text>
-        </View>
-        <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>
-        <View style={styles.buttonContainer}>    
-          <CustomButton buttonAction={this.onLogOutPressed} buttonText={"Log out"}/>
+      <View style={{flex: 1, backgroundColor: Colors.appGrey}}>
+        <View style={styles.container}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 20, marginBottom: 30}}>Settings</Text>
+          <View style={styles.settingOptionWrapper}>
+            <Text style={styles.settingOption}>Banking</Text><Text style={styles.settingArrow}>></Text>
+          </View>
+          <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>
+          <View style={styles.settingOptionWrapper}>
+            <Text style={styles.settingOption}>History</Text><Text style={styles.settingArrow}>></Text>
+          </View>
+          <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>        
+          <View style={styles.settingOptionWrapper}>
+            <Text style={styles.settingOption}>General</Text><Text style={styles.settingArrow}>></Text>
+          </View>
+          <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>        
+          <View style={styles.settingOptionWrapper}>
+            <Text style={styles.settingOption}>Help</Text><Text style={styles.settingArrow}>></Text>
+          </View>
+          <View style={{height: 1, width: '100%', backgroundColor: "black"}}/>
+          <View style={styles.buttonContainer}>    
+            <CustomButton buttonAction={this.onLogOutPressed} buttonText={"Log out"}/>
+          </View>
         </View>
       </View>
     );
@@ -56,19 +58,12 @@ export default connect(null, mapDispatchToProps)(Settings)
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 20,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,    
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
     padding: 15,
     paddingTop: 0,
-    marginTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    backgroundColor: Colors.appWhite,
   },
   settingOption: {
     fontSize: 16,
@@ -77,8 +72,10 @@ const styles = StyleSheet.create({
     marginTop: 10,    
   },
   settingArrow: {
-    color: 'lightgreen',
-    fontSize: 18,    
+    color: Colors.appGreen,
+    fontSize: 22,
+    paddingTop: 6,
+    fontWeight: 'bold'
   },
   settingOptionWrapper: {
     display: 'flex',
