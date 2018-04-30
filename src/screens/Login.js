ceb3d7fac6efca495afae4acdc00a84161b4b821
Roman <r.levyts@gmail.com>
2018-04-30 01:13:11 -0400

    refactor login screen to use customButton component

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -14,9 +14,10 @@ import { connect, dispatch } from 'react-redux';
 import { login } from '../actions/index';
 import axios from "axios";
 
+import CustomButton from '../components/CustomButton';
 import GoBackButton from '../components/GoBackButton';
 import { Colors } from '../constants/styles';
-
+import InputWithIcon from '../components/InputWithIcon';
 
 type Props = {};
 class Login extends Component<Props> {
@@ -53,31 +54,19 @@ class Login extends Component<Props> {
             />
             <Text style={{color: 'red', fontSize: 12}}>{this.props.error ? "username or password don't match" : null}</Text>
             <View>
-              <View style={styles.inputBox}>
-                <Image
-                  style={styles.inputImage}
-                  source={{uri: "https://cdn3.iconfinder.com/data/icons/sympletts-part-3/128/circle-user-man-512.png"}}
-                />
-                <TextInput
-                  placeholder="Username"
-                  onChangeText={ username => this.setState({username})}
-                  value={this.state.username}
-                  underlineColorAndroid='rgba(0,0,0,0)'
-                  style={styles.input}/>
-              </View>
-              <View style={styles.inputBox}>
-                <Image
-                  style={styles.inputImage}
-                  source={{uri: "https://cdn1.iconfinder.com/data/icons/circle-outlines/512/Lock_Locked_Password_Protected_Secure_Safe_Security-512.png"}}
-                />
-                <TextInput
-                  placeholder="Password"          
-                  onChangeText={ password => this.setState({password})}
-                  value={this.state.password}     
-                  style={styles.input} 
-                  secureTextEntry={true}
-                  underlineColorAndroid='rgba(0,0,0,0)'/>
-              </View>
+              <InputWithIcon
+                iconUrl="https://cdn3.iconfinder.com/data/icons/sympletts-part-3/128/circle-user-man-512.png"
+                placeholder="username"
+                inputValue={this.state.username}
+                onInputChange={username => this.setState({username})}
+              />
+              <InputWithIcon
+                iconUrl="https://cdn1.iconfinder.com/data/icons/circle-outlines/512/Lock_Locked_Password_Protected_Secure_Safe_Security-512.png"
+                placeholder="password"
+                secureTextEntry={true}
+                inputValue={this.state.password}
+                onInputChange={password => this.setState({password})}
+              />
             </View>
             <TouchableOpacity
               style={{marginTop: 20}}
@@ -85,11 +74,11 @@ class Login extends Component<Props> {
               >
               <Text>Forgot your username/password?</Text>
             </TouchableOpacity>
-            <TouchableOpacity 
-              onPress={this.onLoginPress}
-              style={styles.button}>
-              <Text style={{fontSize: 20, color: 'white', textAlign: 'center' }}>Log in</Text>
-            </TouchableOpacity>
+            <CustomButton
+              buttonStyles={{marginTop: 20}}
+              buttonAction={this.onLoginPress}
+              buttonText={'Log in'}
+            />
           </KeyboardAvoidingView>
         </View>
       );
@@ -115,36 +104,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.appWhite,
     borderRadius: 15
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  input: {
-    width: '90%',
-    height: 40
-  },
-  inputBox: {
-    display: 'flex',
-    flexDirection: 'row',
-    borderBottomWidth: 1.5,
-    borderBottomColor: 'rgb(65,65,67)',
-    marginLeft: '5%',
-    marginRight: '5%',
-    marginBottom: 20
-  },
-  button: {
-    marginTop: 20,
-    justifyContent: 'center',
-    backgroundColor: 'rgb(65,65,67)',
-    height: 40,
-    width: '90%',
-    borderRadius: 20
-  },
-  inputImage: {
-    height: 30, 
-    width: 30, 
-    resizeMode: "cover"
   }
 });
